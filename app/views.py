b52d09537c5345b56ec9fb354d2f0a3bef51948a
Oliver Gaskell <olivergaskell700@gmail.com>
2020-11-26 23:16:21 +0000

    Views.py didn't move. Don't ask me why.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
+"""Functions to render pages."""
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """Index page on site."""
+    return render(request, 'baseconverter/index.html', {})
--- baseconverter/app/views.py
+++
@@ -1,8 +0,0 @@
-"""Functions to render pages."""
-
-from django.shortcuts import render
-
-
-def index(request):
-    """Index page on site."""
-    return render(request, 'baseconverter/index.html', {})
