602a5e8e0fd809987cc5dae295abfc6e3ca96c40
Oliver Gaskell <olivergaskell700@gmail.com>
2020-11-27 09:17:52 +0000

    Cleaning up code.

---
+++ baseconverter/__init__.py
@@ -0,0 +1,4 @@
+"""Init function for baseconverter.
+
+Allows the baseconverter app to be recognised as a package.
+"""
--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    """Execute a given administrative task."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseconverter.settings')
     try:
         from django.core.management import execute_from_command_line
