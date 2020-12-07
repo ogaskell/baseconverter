9c5e2e5478e130739179582383076787084b7298
Oliver Gaskell <olivergaskell700@gmail.com>
2020-12-07 16:47:55 +0000

    Setup convert page and basic form with placeholders

--- app/views.py
+++ app/views.py
@@ -1,8 +1,27 @@
 """Functions to render pages."""
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+
+bases = {"bin": "2 - Binary",
+         "dec": "10 - Decimal",
+         "hex": "16 - Hexadecimal"}
 
 def index(request):
     """Index page on site."""
     return render(request, 'app/index.html', {})
+
+
+def convert(request):
+    """Page to convert numbers."""
+    if request.method == "GET":
+        return redirect("index")
+    elif request.method == "POST":
+        inbase = request.POST.get('sbase', '')
+        inputv = request.POST.get('in', '')
+        outbase = request.POST.get('tbase', '')
+
+        return render(request, 'app/convert.html', {"input":      str(inputv),
+                                                    "inbase":     bases[str(inbase)],
+                                                    "result":     "80",
+                                                    "resultbase": bases[str(outbase)]})
